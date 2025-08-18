7d6ed6d9e7b765aeafa74cbe9c80420deef49816
phm0423 <qkrgusalsok@gmail.com>
2025-08-18 09:56:56 +0900

    Update EntryView.xaml.cs

--- client/Views/EntryView.xaml.cs
+++ client/Views/EntryView.xaml.cs
@@ -1,10 +1,26 @@
+using client.Services;
+
 namespace client.Views;
 
 public partial class EntryView : ContentPage
 {
-	public EntryView(ViewModels.EntryViewModel vm)
+	private readonly IOrientationService _orientation;
+	public EntryView(ViewModels.EntryViewModel vm, IOrientationService orientation)
 	{
 		InitializeComponent();
 		BindingContext = vm;
+		_orientation = orientation;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _orientation.LockPortrait();
+    }
+
+    protected override void OnDisappearing()
+    {
+        _orientation.UnLock();
+        base.OnDisappearing();
+    }
 }
