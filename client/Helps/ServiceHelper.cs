8f3982ee249a4f212452e241638432e5334b38c0
GhwangHo-Kwon <hoo01041@gmail.com>
2025-08-20 14:45:30 +0900

    [fix] CameraConnect
    카메라 라이브 스트리밍 수정

---
+++ client/Helps/ServiceHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui;
+
+namespace client.Helps;
+
+public static class ServiceHelper
+{
+    private static IServiceProvider? _services;
+    public static IServiceProvider Services =>
+        _services ?? throw new InvalidOperationException("ServiceHelper not initialized.");
+
+    internal static void Initialize(IServiceProvider services) => _services = services;
+}
