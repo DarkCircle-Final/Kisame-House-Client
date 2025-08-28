a6315bcc56b06c06f52105f13fd43bd544d637d4
pgh157 <kh813love@naver.com>
2025-08-28 13:19:19 +0900

    jib

---
+++ client/Converters/StringEqualsConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace client.Converters
+{
+    public class StringEqualsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value?.ToString() == parameter?.ToString();
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => (bool)value ? parameter?.ToString() : Binding.DoNothing;
+    }
+}
